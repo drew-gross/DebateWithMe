74444b86dbd4df823d1ba5cb94c4515b86b6cdcf
Drew Gross <drew.a.gross@gmail.com>
2011-06-08 00:53:08 -0400

    background color based on total supports

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -4,4 +4,15 @@ class Debate < ActiveRecord::Base
   has_many :supports, :dependent => :destroy
   has_many :contests, :dependent => :destroy
 
+  def total_supports
+    self.supports.count - self.contests.count
+  end 
+  
+  def bg_color
+    if self.total_supports > 0
+      [0x00, 0xFF - self.total_supports * 16].max.to_s(16) + "FFFF"
+    else
+      "FFFF" + [0x00, 0xFF - self.total_supports.abs * 16].max.to_s(16)
+    end
+  end 
 end
