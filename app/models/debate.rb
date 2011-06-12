2003d72e419813b1cab706c0eb64c4aaba59440a
Drew Gross <drew.a.gross@gmail.com>
2011-06-12 04:13:13 -0400

    fixed layout of debate/show, messed with forms a bit

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -1,4 +1,28 @@
 class Debate < ActiveRecord::Base
   belongs_to :debate
   has_many :debates, :dependent => :destroy
+  
+  def display_class
+    if is_supporting == true
+      return "supporting"
+    elsif is_supporting == false
+      return "refuting"
+    else
+      return "blank"
+    end
+  end
+
+  def support_count
+    count = debates.inject(0) {|child| child.support_count}
+    if is_supporting == true
+      return count + 1
+    elsif is_supporting == false
+      return count - 1
+    end
+    return count
+  end
+
+  def bg_color
+    "FFFFFF"
+  end
 end
