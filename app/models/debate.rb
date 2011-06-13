be50d5f93f122dc158d01319d6545fa5cde6a6d9
Drew Gross <drew.a.gross@gmail.com>
2011-06-12 23:05:36 -0400

    set bg color based on who is winning

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -13,7 +13,7 @@ class Debate < ActiveRecord::Base
   end
 
   def support_count
-    count = debates.inject(0) {|child| child.support_count}
+    count = debates.inject(0) {|result, child| result + child.support_count}
     if is_supporting == true
       return count + 1
     elsif is_supporting == false
@@ -22,7 +22,25 @@ class Debate < ActiveRecord::Base
     return count
   end
 
+  def subtree_count
+    return debates.inject(0) {|result, child| result + child.subtree_count} + debates.count
+  end
+
   def bg_color
-    "FFFFFF"
+    r = 255
+    g = 255
+    b = 255
+    if subtree_count != 0
+      if support_count > 0
+        r = (subtree_count - support_count) * 255 / subtree_count
+      else
+        b = (subtree_count + support_count) * 255 / subtree_count
+      end
+    end
+    "rgb(" + r.to_s + ", " + g.to_s + ", " + b.to_s + ")"
+  end
+
+  def generate_style
+    "style=\"background-color:" + bg_color + ";\""
   end
 end
